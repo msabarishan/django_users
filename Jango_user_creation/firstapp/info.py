7770a7b942fec0412aea270b362c5d76297946ff
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-06-11 17:06:01 +0530

    Update info.py

--- Jango_user_creation/firstapp/info.py
+++ Jango_user_creation/firstapp/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sabarishanenglish@gmail.com'
-EMAIL_HOST_PASSWORD = 'vzyyxctcpaptjcxe'
+EMAIL_HOST = 'smtp.gmail.com' # For Gmail accounts
+EMAIL_HOST_USER = 'XXXXXXXX@gmail.com' # Create or use existing e-mail ID
+EMAIL_HOST_PASSWORD = 'xhjsdskdksndsf' # create app password using gmail account and paste here
 EMAIL_PORT = 587
